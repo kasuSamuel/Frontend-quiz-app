ce4f9a54342a54c3605e77c76409682f5ff2071a
Kasu Samuel <kasusamuel17@gmail.com>
2024-12-02 11:39:01 +0000

    Navigation method implemented

--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { QuizItem } from '../layout';
+import { CommonModule } from '@angular/common';
+import { QuestionPageComponent } from '../question-page/question-page.component';
 @Component({
   selector: 'app-first-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, QuestionPageComponent],
   templateUrl: './first-page.component.html',
   styleUrl: './first-page.component.css'
 })
-export class FirstPageComponent {
 
+
+export class FirstPageComponent implements OnInit {
+  quizzes: QuizItem[] = [];
+  showQuestion: boolean = false;
+
+  constructor(private dataService: DataService) {
+    this.dataService.getQuizzes().subscribe((quizzes: QuizItem[]) => {
+      this.quizzes =  quizzes;
+      console.log(this.quizzes);
+
+    })
+  }
+  ngOnInit(): void {
+  }
+
+  
+  goToQuestion() {
+    this.showQuestion = true;
+  }
+
+  goToHome() {
+    this.showQuestion = false;
+  }
 }
--- src/app/question-page/question-page.component.ts
+++ src/app/question-page/question-page.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { QuizItem, Question } from '../layout';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-question-page',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule,],
   templateUrl: './question-page.component.html',
   styleUrl: './question-page.component.css'
 })
-export class QuestionPageComponent {
+export class QuestionPageComponent implements OnInit{
+  quizzes: QuizItem[] = [];
+  
 
+  constructor(private dataService: DataService) {
+    this.dataService.getQuizzes().subscribe((quizzes: QuizItem[]) => {
+      this.quizzes =  quizzes;
+      console.log(this.quizzes);
+      console.log(this.quizzes[0].questions);
+    })
+  }
+  ngOnInit(): void {
+  }
 }
